d9ef1bb802dc259c2b46fc5c3b9eaa8915088047
rrocamar <r.roca@alumnos.upm.es>
2018-10-05 11:01:13 +0200

    Creada clase C31Test

---
+++ src/test/java/es/upm/miw/C31Test.java
@@ -0,0 +1,20 @@
+package es.upm.miw;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class C31Test {
+
+    private C31 c31 = new C31();
+
+    @Test
+    void testM1() {
+        assertEquals("m1", c31.m1());
+    }
+
+    @Test
+    void testM2() {
+        assertEquals("m2", c31.m2());
+    }
+}
